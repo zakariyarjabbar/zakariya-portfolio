805bb2a2bcddd25ffde6b86284df10271918f346
Zakariya Jabbar <zakariya.r.jabbar@gmail.com>
2026-09-23 14:00:04 +0300

    Revert "Scale up hero portrait"
    This reverts commit 7a6e9efb0c7b4554d7f3c6ab120d69e6efacd4e1.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ export function Hero() {
           src={heroPortrait}
           alt="Black-and-white portrait of Zakariya Jabbar"
           priority
-          sizes="(max-width: 820px) min(141vw, 720px), (max-width: 980px) 66vw, (max-width: 1100px) 69vw, clamp(735px, 72vw, 1020px)"
+          sizes="(max-width: 820px) min(94vw, 480px), (max-width: 980px) 44vw, (max-width: 1100px) 46vw, clamp(490px, 48vw, 680px)"
           draggable={false}
           className="hero__portrait-image select-none"
         />
